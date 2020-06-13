1a5c1296b4c45c2f3967c3e6ca3f9a4a11037cc3
aleksandar9999a <aleksandar9999a@gmail.com>
2020-06-13 15:50:53 +0300

    Remove keys

--- src/config/firebase.tsx
+++ src/config/firebase.tsx
@@ -4,14 +4,14 @@ import 'firebase/firestore';
 import 'firebase/storage';
 
 const fbConfig = {
-    apiKey: "AIzaSyAMEvdXlDDLzzI4IJi60tDv4QFHj6mHf3s",
-    authDomain: "experience-intercity-jouney.firebaseapp.com",
-    databaseURL: "https://experience-intercity-jouney.firebaseio.com",
-    projectId: "experience-intercity-jouney",
-    storageBucket: "experience-intercity-jouney.appspot.com",
-    messagingSenderId: "699755951072",
-    appId: "1:699755951072:web:8b49294602abd47742b185",
-    measurementId: "G-VFJ6BYFFWY"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 }
 
 export const firebase = fb.initializeApp(fbConfig);
