cdf11a1633b3dfb4de8650190fb5b2066cf57138
aleksandar9999a <aleksandar9999a@gmail.com>
2020-06-13 09:07:32 +0300

    Added pwa-elements

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+defineCustomElements(window);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
