cffef834f469b8e1812712090508bbbf07ad31d3
aleksandar9999a <aleksandar9999a@gmail.com>
2020-06-13 15:19:08 +0300

    Fix submitRegistered, added more messages

--- src/services/auth.tsx
+++ src/services/auth.tsx
@@ -3,19 +3,23 @@ import IRegistered from '../interfaces/IRegistered';
 import { submitMessage } from './toast';
 
 export function submitRegistered(data: IRegistered) {
-    return auth.createUserWithEmailAndPassword(data.email, data.password).then((res) => {
-        const userdata = {
-            email: data.email,
-            city: data.city,
-            firstName: data.firstName,
-            lastName: data.lastName
-        }
-        return firestore().collection('users').doc(res.user?.uid).set(userdata);
-    }).catch(err => submitMessage(err.message));
+    return auth.createUserWithEmailAndPassword(data.email, data.password)
+        .then(res => {
+            const userdata = {
+                uid: res.user?.uid,
+                email: data.email,
+                city: data.city,
+                firstName: data.firstName,
+                lastName: data.lastName
+            }
+            return firestore.collection('users').doc(res.user?.uid).set(userdata);
+        })
+        .then(res => submitMessage('Successful Registered!'))
+        .catch(err => submitMessage(err.message));
 }
 
 export function submitLogin(email: string, password: string) {
-    return auth.signInWithEmailAndPassword(email, password).catch(err => submitMessage(err.message));
+    return auth.signInWithEmailAndPassword(email, password).then(res => submitMessage('Successful Login!')).catch(err => submitMessage(err.message));
 }
 
 export function logOut() {
