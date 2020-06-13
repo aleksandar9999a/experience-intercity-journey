2fc35f68b0dcf406706b699282c3d3b37cfb47ea
aleksandar9999a <aleksandar9999a@gmail.com>
2020-06-13 14:40:36 +0300

    Added full login functionality

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,21 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IonCard, IonCardContent, IonInput, IonLabel, IonItem, IonButton } from '@ionic/react';
 import './style.css';
+import { submitLogin } from '../../services/auth';
+import { submitMessage } from '../../services/toast';
+import isEmail from 'validator/lib/isEmail';
 
 const Login: React.FC = () => {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  function validate() {
+    if (!isEmail(email)) {
+      submitMessage('Email format is invalid!');
+      return false;
+    }
+    if (password.length < 8 || password.length > 20) {
+      submitMessage('Password is invalid. Minimum length is 8 chars, max - 20 chars.');
+      return false;
+    }
+    return true;
+  }
+
+  function submit() { if (!validate()) { return; } submitLogin(email, password); }
+  function handleEmail(e: any) { setEmail(e.target.value); }
+  function handlePassword(e: any) { setPassword(e.target.value); }
+
   return (
     <IonCard color="light">
       <IonCardContent>
         <IonItem color="light">
           <IonLabel position="floating">Email</IonLabel>
-          <IonInput type="email" required={true} pattern="email"></IonInput>
+          <IonInput type="email" value={email} onIonChange={handleEmail}></IonInput>
         </IonItem>
         <IonItem color="light">
           <IonLabel position="floating">Password</IonLabel>
-          <IonInput type="password" maxlength={20} minlength={8} required={true} ></IonInput>
+          <IonInput type="password" value={password} onIonChange={handlePassword}></IonInput>
         </IonItem>
         <div className="ion-padding">
-          <IonButton expand="block" fill="solid" color="success" type="submit">LogIn</IonButton>
+          <IonButton expand="block" fill="solid" color="success" onClick={submit}>LogIn</IonButton>
         </div>
         <IonItem routerLink="/register" className="register-route" color="light">
           <IonLabel color="secondary">Don't have an account? Register</IonLabel>
