12f4798ef4d5bcd59eeeb676b8cd1db323a5375f
aleksandar9999a <aleksandar9999a@gmail.com>
2020-06-13 14:38:57 +0300

    Added firebase config

---
+++ src/config/firebase.tsx
@@ -0,0 +1,20 @@
+import fb from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firebase';
+import 'firebase/storage';
+
+const fbConfig = {
+    apiKey: "AIzaSyAMEvdXlDDLzzI4IJi60tDv4QFHj6mHf3s",
+    authDomain: "experience-intercity-jouney.firebaseapp.com",
+    databaseURL: "https://experience-intercity-jouney.firebaseio.com",
+    projectId: "experience-intercity-jouney",
+    storageBucket: "experience-intercity-jouney.appspot.com",
+    messagingSenderId: "699755951072",
+    appId: "1:699755951072:web:8b49294602abd47742b185",
+    measurementId: "G-VFJ6BYFFWY"
+}
+
+export const firebase = fb.initializeApp(fbConfig);
+export const auth = firebase.auth();
+export const firestore = firebase.firestore;
+export const storage = firebase.storage();
