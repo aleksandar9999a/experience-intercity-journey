310fd9566e82867069dd14ed6a81c33990f6b3c9
aleksandar9999a <aleksandar9999a@gmail.com>
2020-06-13 12:02:25 +0300

    Search layout

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { IonCard, IonCardContent, IonItem, IonLabel, IonInput, IonButton } from '@ionic/react';
 
 const Search: React.FC = () => {
-  return (<div>Search</div>);
+  return (
+    <IonCard color="light">
+    <IonCardContent>
+      <IonItem color="light">
+        <IonLabel position="floating">From</IonLabel>
+        <IonInput type="text" required={true}></IonInput>
+      </IonItem>
+      <IonItem color="light">
+        <IonLabel position="floating">To</IonLabel>
+        <IonInput type="text" maxlength={20} minlength={8} required={true}></IonInput>
+      </IonItem>
+      <div className="ion-padding">
+        <IonButton expand="block" fill="solid" color="success" type="submit">Search</IonButton>
+      </div>
+    </IonCardContent>
+  </IonCard>
+  );
 };
 
 export default Search;
