cac691b4c35127ba6654470ba0336a9a0b453f27
aleksandar9999a <aleksandar9999a@gmail.com>
2020-06-13 15:44:24 +0300

    Added logout item

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,9 +1,11 @@
-import { IonContent, IonList, IonListHeader, IonMenu, IonNote } from '@ionic/react';
+import { IonContent, IonList, IonListHeader, IonMenu, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel } from '@ionic/react';
 import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import menu_config from '../../config/menu_config';
 import MenuItem from '../MenuItem';
 import './style.css';
+import { chevronBackOutline, chevronBackSharp } from 'ionicons/icons';
+import { logOut } from './../../services/auth';
 
 
 const Menu: React.FC = () => {
@@ -15,6 +17,8 @@ const Menu: React.FC = () => {
     setList(newList);
   }, [location])
 
+  function out() { logOut(); }
+
   return (
     <IonMenu contentId="main" type="overlay">
       <IonContent>
@@ -22,6 +26,12 @@ const Menu: React.FC = () => {
           <IonListHeader>Inbox</IonListHeader>
           <IonNote>hi@ionicframework.com</IonNote>
           {list}
+          <IonMenuToggle autoHide={false}>
+            <IonItem routerDirection="none" lines="none" detail={false} onClick={out}>
+                <IonIcon slot="start" ios={chevronBackOutline} md={chevronBackSharp} />
+                <IonLabel>Log Out</IonLabel>
+            </IonItem>
+        </IonMenuToggle>
         </IonList>
       </IonContent>
     </IonMenu>
