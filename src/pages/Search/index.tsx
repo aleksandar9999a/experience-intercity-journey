724fca0c125b7954a24b51919f4d771f5d089315
aleksandar9999a <aleksandar9999a@gmail.com>
2020-06-14 12:03:57 +0300

    Added grid

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,23 +1,29 @@
 import React from 'react';
-import { IonCard, IonCardContent, IonItem, IonLabel, IonInput, IonButton } from '@ionic/react';
+import { IonCard, IonCardContent, IonInput, IonButton, IonGrid, IonRow, IonCol } from '@ionic/react';
 
 const Search: React.FC = () => {
   return (
     <IonCard color="light">
-    <IonCardContent>
-      <IonItem color="light">
-        <IonLabel position="floating">From</IonLabel>
-        <IonInput type="text" required={true}></IonInput>
-      </IonItem>
-      <IonItem color="light">
-        <IonLabel position="floating">To</IonLabel>
-        <IonInput type="text" maxlength={20} minlength={8} required={true}></IonInput>
-      </IonItem>
-      <div className="ion-padding">
-        <IonButton expand="block" fill="solid" color="success" type="submit">Search</IonButton>
-      </div>
-    </IonCardContent>
-  </IonCard>
+      <IonCardContent>
+        <IonGrid>
+          <IonRow>
+            <IonCol>
+              <IonInput placeholder="From" type="text" required={true}></IonInput>
+            </IonCol>
+            <IonCol>
+              <IonInput placeholder="To" type="text" maxlength={20} minlength={8} required={true}></IonInput>
+            </IonCol>
+          </IonRow>
+          <IonRow className="ion-align-items-center">
+            <IonCol></IonCol>
+            <IonCol className="ion-align-self-center">
+              <IonButton fill="solid" color="success">Search</IonButton>
+            </IonCol>
+            <IonCol></IonCol>
+          </IonRow>
+        </IonGrid>
+      </IonCardContent>
+    </IonCard>
   );
 };
 
