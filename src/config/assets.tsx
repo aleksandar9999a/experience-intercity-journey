2969a29f7df854e1a141edb3c15abb06410b322b
aleksandar9999a <aleksandar9999a@gmail.com>
2020-06-15 13:44:22 +0300

    Added assets with images for create publication

---
+++ src/config/assets.tsx
@@ -0,0 +1,8 @@
+const assets = {
+    when: require('./../assets/when.jpg'),
+    start: require('./../assets/start.jpg'),
+    kindOfTransport: require('./../assets/kindOfTransport.jpg'),
+    startJourney: require('./../assets/startJourney.jpg'),
+}
+
+export default assets;
--- src/pages/CreatePublication/index.tsx
+++ src/pages/CreatePublication/index.tsx
@@ -6,6 +6,7 @@ import TCreateState from '../../types/TCreateState';
 import { submitMessage } from '../../services/toast';
 import isAfter from 'validator/lib/isAfter';
 import { setPublication } from '../../services';
+import assets from './../../config/assets';
 
 const CreatePublication: React.FC = () => {
   const [from, setFrom] = useState<string>('');
@@ -47,16 +48,20 @@ const CreatePublication: React.FC = () => {
     <IonSlides>
       <IonSlide>
         <div className="slide">
-          <img src="https://www.kindpng.com/picc/m/65-652552_clipart-bee-signboard-cartoon-sign-board-png-transparent.png" alt="create-publicatio" />
-          <IonItem >
-            <IonLabel position="floating">From</IonLabel>
-            <IonInput type="text" value={from} onIonChange={handleFrom} />
-          </IonItem>
-          <IonItem >
-            <IonLabel position="floating">To</IonLabel>
-            <IonInput type="text" value={to} onIonChange={handleTo} />
-          </IonItem>
-          <p>Enter your start and end location!</p>
+          <div className="slide-img">
+            <img src={assets.start} alt="create-publication" />
+            <div><h1>Where will you travel?</h1></div>
+          </div>
+          <div>
+            <IonItem >
+              <IonLabel position="floating">From</IonLabel>
+              <IonInput type="text" value={from} onIonChange={handleFrom} />
+            </IonItem>
+            <IonItem >
+              <IonLabel position="floating">To</IonLabel>
+              <IonInput type="text" value={to} onIonChange={handleTo} />
+            </IonItem>
+          </div>
           <div className='slide-right'>
             <p>Slide Right</p>
           </div>
@@ -64,16 +69,20 @@ const CreatePublication: React.FC = () => {
       </IonSlide>
       <IonSlide>
         <div className="slide">
-          <img src="https://www.kindpng.com/picc/m/65-652552_clipart-bee-signboard-cartoon-sign-board-png-transparent.png" alt="create-publicatio" />
-          <IonItem>
-            <IonLabel>Date</IonLabel>
-            <IonDatetime placeholder="Select Date" value={date} onIonChange={handleDate}></IonDatetime>
-          </IonItem>
-          <IonItem>
-            <IonLabel>Time</IonLabel>
-            <IonDatetime placeholder="Select Time" display-format="h:mm A" picker-format="h:mm A" value={time} onIonChange={handleTime}></IonDatetime>
-          </IonItem>
-          <p>Enter the date you will travel!</p>
+          <div className="slide-img">
+            <img src={assets.when} alt="create-publication" />
+            <div><h1>When are you travelling?</h1></div>
+          </div>
+          <div>
+            <IonItem>
+              <IonLabel>Date</IonLabel>
+              <IonDatetime placeholder="Select Date" value={date} onIonChange={handleDate}></IonDatetime>
+            </IonItem>
+            <IonItem>
+              <IonLabel>Time</IonLabel>
+              <IonDatetime placeholder="Select Time" display-format="h:mm A" picker-format="h:mm A" value={time} onIonChange={handleTime}></IonDatetime>
+            </IonItem>
+          </div>
           <div className='slide-right'>
             <p>Slide Right</p>
           </div>
@@ -81,15 +90,19 @@ const CreatePublication: React.FC = () => {
       </IonSlide>
       <IonSlide>
         <div className="slide">
-          <img src="https://www.kindpng.com/picc/m/65-652552_clipart-bee-signboard-cartoon-sign-board-png-transparent.png" alt="create-publicatio" />
-          <IonItem >
-            <IonLabel>Search</IonLabel>
-            <IonSelect interface="action-sheet" value={type} onIonChange={handleType}>
-              <IonSelectOption value="transport">Transport</IonSelectOption>
-              <IonSelectOption value="drive">To Drive</IonSelectOption>
-            </IonSelect>
-          </IonItem>
-          <p>Choose type of search!</p>
+          <div className="slide-img">
+            <img src={assets.kindOfTransport} alt="create-publication" />
+            <div><h1>What kind of transport are you looking for?</h1></div>
+          </div>
+          <div>
+            <IonItem >
+              <IonLabel>Search</IonLabel>
+              <IonSelect interface="action-sheet" value={type} onIonChange={handleType}>
+                <IonSelectOption value="transport">Transport</IonSelectOption>
+                <IonSelectOption value="drive">To Drive</IonSelectOption>
+              </IonSelect>
+            </IonItem>
+          </div>
           <div className='slide-right'>
             <p>Slide Right</p>
           </div>
@@ -97,11 +110,16 @@ const CreatePublication: React.FC = () => {
       </IonSlide>
       <IonSlide>
         <div className="slide last-slide">
-          <img src="https://www.kindpng.com/picc/m/65-652552_clipart-bee-signboard-cartoon-sign-board-png-transparent.png" className="create-img" alt="create-publicatio" />
-          <IonButton fill="clear" className="create-btn" onClick={submit}>
-            Create Publication
-            <IonIcon slot="end" ios={arrowForward} md={arrowForward} />
-          </IonButton>
+          <img src={assets.startJourney} className="create-img" alt="create-publicatio" />
+          <div className="start-journey">
+            <div className="start-journey-wrapper">
+              <h1>Start your journey!</h1>
+              <IonButton className="create-btn" onClick={submit}>
+                Create Publication
+                <IonIcon slot="end" ios={arrowForward} md={arrowForward} />
+              </IonButton>
+            </div>
+          </div>
         </div>
       </IonSlide>
     </IonSlides>
