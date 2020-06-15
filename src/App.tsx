64a23407433fa4ef2cd09e7c29d9787cffc30a52
aleksandar9999a <aleksandar9999a@gmail.com>
2020-06-15 16:38:21 +0300

    Refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Menu from './components/Menu';
-import React from 'react';
-import { IonApp, IonRouterOutlet, IonSplitPane } from '@ionic/react';
+import React, { useEffect, useState } from 'react';
+import { IonApp, IonRouterOutlet, IonSplitPane, IonContent, IonLoading, IonPage } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { Redirect, Route } from 'react-router-dom';
 
@@ -17,17 +17,94 @@ import '@ionic/react/css/display.css';
 import './theme/variables.css';
 import './App.css';
 import Page from './pages/Page';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth, firestore } from './config/firebase';
+import IUser from './interfaces/IUser';
+import { submitMessage } from './services/toast';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import FabMenu from './components/FabMenu';
 
 const App: React.FC = () => {
+  const [user, loading, error] = useAuthState(auth);
+  const [userdata, setUserdata] = useState<IUser>();
+
+  useEffect(() => {
+    if (user) {
+      firestore.collection('users').doc(user.uid).onSnapshot(doc => {
+        if (!doc.exists) { submitMessage('No information about user!'); return; }
+        setUserdata(doc.data() as IUser);
+      })
+    }
+  }, [user])
+
+  if (error) {
+    return (
+      <IonApp>
+        <IonSplitPane contentId="main">
+          <IonPage>
+            <IonContent>
+              <div className="error-page">
+                <h1>{error}</h1>
+              </div>
+            </IonContent>
+          </IonPage>
+        </IonSplitPane>
+      </IonApp>
+    );
+  }
+
+  if (loading) {
+    return (
+      <IonApp>
+        <IonSplitPane contentId="main">
+          <IonPage>
+            <IonContent>
+              <IonLoading
+                cssClass='my-custom-class'
+                isOpen={loading}
+                message={'Please wait...'}
+              />
+            </IonContent>
+          </IonPage>
+        </IonSplitPane>
+      </IonApp>
+    )
+  }
+
+  if (!user) {
+    return (
+      <IonApp>
+        <IonReactRouter>
+          <IonSplitPane contentId="main">
+            <IonRouterOutlet id="main">
+              <IonContent>
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/register" component={Register} />
+                <Redirect exact path="*" to="/login" />
+              </IonContent>
+            </IonRouterOutlet>
+          </IonSplitPane>
+        </IonReactRouter>
+      </IonApp>
+    )
+  }
+
   return (
     <IonApp>
       <IonReactRouter>
         <IonSplitPane contentId="main">
-          <Menu />
-          <IonRouterOutlet id="main">
-            <Route path="/:name" component={Page} exact />
-            <Redirect exact path="/" to="/search" />
-          </IonRouterOutlet>
+          <IonPage>
+            <IonContent>
+              <Menu firstName={userdata?.firstName} lastName={userdata?.lastName} image={userdata?.image} />
+              <FabMenu />
+              <IonRouterOutlet id="main">
+                <Route path="/:name" component={Page} exact />
+                <Redirect exact path="/login" to="/search" />
+                <Redirect exact path="/register" to="/search" />
+              </IonRouterOutlet>
+            </IonContent>
+          </IonPage>
         </IonSplitPane>
       </IonReactRouter>
     </IonApp>
--- src/pages/Page/index.tsx
+++ src/pages/Page/index.tsx
@@ -1,64 +1,15 @@
-import { IonContent, IonPage, IonHeader, IonToolbar, IonTitle, IonLoading } from '@ionic/react';
 import React from 'react';
 import { useParams } from 'react-router';
 import { Toast } from '../../components/Toast';
 import CurrentPage from '../CurrentPage';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { auth } from './../../config/firebase';
-import { submitMessage } from '../../services/toast';
-import FabMenu from '../../components/FabMenu';
 
 const Page: React.FC = () => {
     const { name } = useParams<{ name: string; }>();
-    const [user, loading, error] = useAuthState(auth);
-
-    if (error) { submitMessage(error.message); }
-    if (loading) {
-        return (
-            <IonContent>
-                <IonLoading
-                    cssClass='my-custom-class'
-                    isOpen={loading}
-                    message={'Please wait...'}
-                />
-            </IonContent>
-        )
-    }
-    if (!user && name !== 'login' && name !== 'register') {
-        return (
-            <IonPage>
-                <IonContent>
-                    <CurrentPage name='login' />
-                    <Toast />
-                </IonContent>
-            </IonPage>
-        )
-    }
-    if (!user && (name === 'login' || name === 'register')) {
-        return (
-            <IonPage>
-                <IonContent>
-                    <CurrentPage name={name} />
-                    <Toast />
-                </IonContent>
-            </IonPage>
-        )
-    }
-
     return (
-        <IonPage>
-            <FabMenu />
-
-            <IonContent>
-                <IonHeader collapse="condense">
-                    <IonToolbar>
-                        <IonTitle size="large">{name}</IonTitle>
-                    </IonToolbar>
-                </IonHeader>
-                <CurrentPage name={name} />
-                <Toast />
-            </IonContent>
-        </IonPage>
+        <div>
+            <CurrentPage name={name} />
+            <Toast />
+        </div>
     );
 };
 
