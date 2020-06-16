08ed1aeb6616ef39f4918ad27bbfa93ae169b07b
aleksandar9999a <aleksandar9999a@gmail.com>
2020-06-16 12:42:53 +0300

    Added PublicationListItem

---
+++ src/components/PublicationListItem/index.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import { IonItem, IonLabel } from '@ionic/react';
+import IPublication from '../../interfaces/IPublication';
+import './style.css';
+import { getImageByPlace } from '../../services';
+import IPixabayImage from '../../interfaces/IPixabayImage';
+
+const PublicationListItem: React.FC<{ data: IPublication }> = ({ data }) => {
+    const [date, setDate] = useState<string>('');
+    const [image, setImage] = useState<IPixabayImage>();
+
+    useEffect(() => { setDate(new Date(data.date).toLocaleDateString()); }, [data.date])
+    useEffect(() => {
+        getImageByPlace(data.to).then((res) => {
+            if (!res) { return; }
+            const item = res.data.hits[Math.floor(Math.random() * res.data.hits.length)];
+            setImage(item)
+        })
+    }, [data.to])
+
+    return (
+        <IonItem>
+            <div className="list-item-first">
+                {!!image && <img src={image?.previewURL} className="list-item-preview-img" alt="preview-img" />}
+            </div>
+            <div className="list-item-second">
+                <IonLabel>From: {data.from}</IonLabel>
+                <IonLabel>To: {data.to}</IonLabel>
+            </div>
+            <div className="list-item-third">
+                <IonLabel>Type: {data.type}</IonLabel>
+                <IonLabel>Date: {date}</IonLabel>
+            </div>
+        </IonItem>
+    );
+};
+
+export default PublicationListItem;
