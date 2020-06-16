26d2895d5a75532052583d1b72a2585795c9402d
aleksandar9999a <aleksandar9999a@gmail.com>
2020-06-16 12:44:08 +0300

    Remove card, added search, list

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,29 +1,39 @@
-import React from 'react';
-import { IonCard, IonCardContent, IonInput, IonButton, IonGrid, IonRow, IonCol } from '@ionic/react';
+import React, { useEffect, useState } from 'react';
+import { IonSearchbar, IonToolbar, IonList } from '@ionic/react';
+import { getPublications } from '../../services';
+import IPublication from '../../interfaces/IPublication';
+import PublicationListItem from '../../components/PublicationListItem';
 
 const Search: React.FC = () => {
+  const [list, setList] = useState<JSX.Element[]>([]);
+  const [items, setItem] = useState<IPublication[]>([]);
+
+  useEffect(() => {
+    const newList = items.map((x, i) => <PublicationListItem key={i} data={x} />);
+    setList(newList);
+  }, [items])
+
+  useEffect(() => {
+    getPublications({}).then(snapshot => {
+      snapshot.forEach(doc => {
+        setItem((oldItems) => {
+          let newItems= [...oldItems];
+          newItems.push(doc.data() as IPublication);
+          return newItems;
+        })
+      })
+    })
+  }, [])
+
   return (
-    <IonCard color="light">
-      <IonCardContent>
-        <IonGrid>
-          <IonRow>
-            <IonCol>
-              <IonInput placeholder="From" type="text" required={true}></IonInput>
-            </IonCol>
-            <IonCol>
-              <IonInput placeholder="To" type="text" maxlength={20} minlength={8} required={true}></IonInput>
-            </IonCol>
-          </IonRow>
-          <IonRow className="ion-align-items-center">
-            <IonCol></IonCol>
-            <IonCol className="ion-align-self-center">
-              <IonButton fill="solid" color="success">Search</IonButton>
-            </IonCol>
-            <IonCol></IonCol>
-          </IonRow>
-        </IonGrid>
-      </IonCardContent>
-    </IonCard>
+    <div>
+      <IonToolbar>
+        <IonSearchbar placeholder="Where will you travel?" type="tel" showCancelButton="focus" debounce={1000}></IonSearchbar>
+      </IonToolbar>
+      <IonList>
+        {list}
+      </IonList>
+    </div>
   );
 };
 
