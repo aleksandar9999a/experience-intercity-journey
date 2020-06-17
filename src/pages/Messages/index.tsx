730c3511346ca478c5a141aad7d0c353fe3414b8
aleksandar9999a <aleksandar9999a@gmail.com>
2020-06-17 21:34:51 +0300

    Added list of chats

--- src/pages/Messages/index.tsx
+++ src/pages/Messages/index.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getAllMessages } from '../../services';
+import { IonList, IonListHeader } from '@ionic/react';
+import ChatItem from '../../components/ChatItem';
+import './style.css';
+
 
 const Messages: React.FC = () => {
-    
+    const [list, setList] = useState<JSX.Element[]>([]);
+    useEffect(() => {
+        const sub = getAllMessages.subscribe(chats => {
+            const newList = chats.map(chat => <ChatItem key={chat.id} data={chat} />);
+            setList(newList)
+        })
+
+        return () => { sub.unsubscribe(); }
+    }, [])
     return (
-        <div>Messages</div>
+        <IonList>
+            <IonListHeader>
+                <h2 className="chat-box-title">Chat Box</h2>
+            </IonListHeader>
+            {list}
+        </IonList>
     );
 };
 
