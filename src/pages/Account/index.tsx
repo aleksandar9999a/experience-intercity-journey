00dd2aede1000aa86adb5f9a23ab6e6f674df559
aleksandar9999a <aleksandar9999a@gmail.com>
2020-06-17 12:25:32 +0300

    Added MyProfile and different states for loading user and etc.

--- src/pages/Account/index.tsx
+++ src/pages/Account/index.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from './../../config/firebase';
+import { IonLoading } from '@ionic/react';
+import MyProfile from '../../containers/MyProfile';
 
 const Account: React.FC = () => {
-  return (<div>Account</div>);
+  const [user, loading, error] = useAuthState(auth);
+
+  if (loading) { return <IonLoading cssClass='my-custom-class' isOpen={loading} message={'Please wait...'} /> }
+  if (error) { return <div className="error-page"> <h1>{error}</h1></div> }
+  if (!user) { return <div className="error-page"> <h1>Unauthorized</h1></div> }
+
+  return <MyProfile uid={user.uid} />
 };
+
+
 
 export default Account;
