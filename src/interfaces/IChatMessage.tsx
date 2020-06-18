8f98367d11bd7d2db780e02073556cb18edb12a7
aleksandar9999a <aleksandar9999a@gmail.com>
2020-06-18 11:31:58 +0300

    Added new interfaces

---
+++ src/interfaces/IChatMessage.tsx
@@ -0,0 +1,7 @@
+import IMessage from "./IMessage";
+import IUser from "./IUser";
+
+export default interface IChatMessage {
+    chat: IMessage
+    user: IUser
+}
---
+++ src/interfaces/IMessage.tsx
@@ -0,0 +1,6 @@
+export default interface IMessage {
+    created: Date,
+    creatorId: string,
+    id: string,
+    message: string
+}
