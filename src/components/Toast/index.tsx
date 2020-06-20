9c212f467b0bdb8d09162489ae42a7a608ab2a1e
aleksandar9999a <aleksandar9999a@gmail.com>
2020-06-20 09:58:07 +0300

    Refactoring

--- src/components/Toast/index.tsx
+++ src/components/Toast/index.tsx
@@ -3,8 +3,8 @@ import { IonToast } from '@ionic/react';
 import { getToastStore, close } from '../../services/toast';
 import IToastStore from '../../interfaces/IToastStore';
 
-export const Toast: React.FC = () => {
-  const [state, setState] = useState<IToastStore>();
+const Toast: React.FC = () => {
+  const [state, setState] = useState<IToastStore>({ isOpen: false, message: '' });
 
   useEffect(() => {
     const sub = getToastStore().subscribe(setState);
@@ -14,11 +14,13 @@ export const Toast: React.FC = () => {
   function removeMessage() { close(); }
 
   return (
-      <IonToast
-        isOpen={state?.isOpen || false}
-        onDidDismiss={removeMessage}
-        message={state?.message}
-        duration={3000}
-      />
+    <IonToast
+      isOpen={state.isOpen || false}
+      onDidDismiss={removeMessage}
+      message={state?.message}
+      duration={3000}
+    />
   );
 };
+
+export default Toast;
