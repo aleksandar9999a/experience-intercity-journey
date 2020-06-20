8e85346bd70d8d1b7e59b909463a480387839857
aleksandar9999a <aleksandar9999a@gmail.com>
2020-06-20 10:54:16 +0300

    Refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import React, { useEffect } from 'react';
 import { IonApp, IonContent } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { auth } from './config/firebase';
 import { myUserdata } from './services';
 import MenuContainer from './containers/MenuContainer';
-import ErrorPage from './pages/ErrorPage';
-import LoadingPage from './pages/LoadingPage';
 import Outlet from './containers/Outlet';
 import './Styles';
 
 const App: React.FC = () => {
-  const [_, loading, error] = useAuthState(auth);
 
   useEffect(() => {
     const sub = myUserdata.subscribe((userdata) => {
@@ -21,9 +16,6 @@ const App: React.FC = () => {
 
     return () => { sub.unsubscribe(); }
   }, [])
-
-  if (loading) { return <LoadingPage isOpen={loading} /> }
-  if (error) { return <ErrorPage message={error.message} /> }
 
   return (
     <IonApp>
--- src/containers/Outlet/index.tsx
+++ src/containers/Outlet/index.tsx
@@ -1,23 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { IonRouterOutlet } from '@ionic/react';
 import { Redirect, Route } from 'react-router-dom';
 import route_config from '../../config/route_config';
-import { myUserdata } from '../../services';
-import IUser from '../../interfaces/IUser';
 import ICustomRoute from '../../interfaces/ICustomRoute';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '../../config/firebase';
+import LoadingPage from '../../pages/LoadingPage';
+import ErrorPage from '../../pages/ErrorPage';
 
 const Outlet: React.FC = () => {
-    const [user, setUser] = useState<IUser | null>();
+    const [user, loading, error] = useAuthState(auth);
     const routes = route_config.map(generateRoute);
-
-    useEffect(() => {
-        const sub = myUserdata.subscribe((userdata) => setUser(userdata));
-        return () => { sub.unsubscribe(); }
-    }, [])
 
     function generateRoute({ path, component }: ICustomRoute, index: number) {
         return <Route key={index} path={path} component={component} exact />
     }
+
+    if (loading) { return <LoadingPage isOpen={loading} /> }
+    if (error) { return <ErrorPage message={error.message} /> }
 
     return (
         <IonRouterOutlet id="main">
