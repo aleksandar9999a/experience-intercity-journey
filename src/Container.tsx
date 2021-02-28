25981de3fa9c21d4fb15d8cf4d980e643b8212e8
Alexander Todorov <aleksandar9999a@gmail.com>
2021-02-28 12:27:49 +0200

    Added new binds

--- src/Container.tsx
+++ src/Container.tsx
@@ -1,6 +1,8 @@
 import { Container } from 'inversify';
 import { PixabayConfig } from './config/PixabayConfig';
+import { AuthManager } from './services/AuthManager';
 import { ErrorManager } from './services/ErrorManager';
+import { MessageManager } from './services/MessageManager';
 import { Pixabay } from './services/PixabayManager';
 import { RouterManager } from './services/RouterManager';
 import type from './Types';
@@ -11,5 +13,7 @@ container.bind<RouterManager>(type.RouterManager).to(RouterManager);
 container.bind<Pixabay>(type.Pixabay).to(Pixabay);
 container.bind<PixabayConfig>(type.PixabayConfig).to(PixabayConfig);
 container.bind<ErrorManager>(type.ErrorManager).to(ErrorManager);
+container.bind<AuthManager>(type.AuthManager).to(AuthManager);
+container.bind<MessageManager>(type.MessageManager).to(MessageManager);
 
 export const routerManager = container.resolve(RouterManager)
