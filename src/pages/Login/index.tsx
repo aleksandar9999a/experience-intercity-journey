f06f0b838d1cfc176ed88f7aafe78b1d3a33604c
Alexander Todorov <aleksandar9999a@gmail.com>
2021-02-28 12:29:18 +0200

    Refactoring, added authManager

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -6,8 +6,10 @@ import isEmail from 'validator/lib/isEmail';
 import { arrowForward } from 'ionicons/icons';
 import Logo from '../../components/Logo';
 import './style.css';
+import { ILoginProps } from '../../interfaces/interfaces';
+import { observer } from 'mobx-react';
 
-const Login: React.FC = () => {
+export const Login = observer(({ authManager }: ILoginProps) => {
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
 
@@ -16,41 +18,66 @@ const Login: React.FC = () => {
       submitMessage('Email format is invalid!');
       return false;
     }
+
     if (password.length < 8 || password.length > 20) {
       submitMessage('Password is invalid. Minimum length is 8 chars, max - 20 chars.');
       return false;
     }
+
     return true;
   }
 
-  function submit() { if (!validate()) { return; } submitLogin(email, password); }
-  function handleEmail(e: any) { setEmail(e.target.value); }
-  function handlePassword(e: any) { setPassword(e.target.value); }
+  function submit() {
+    if (!validate()) {
+      return;
+    }
+
+    authManager.login(email, password);
+  }
+
+  function handleEmail(e: any) {
+    setEmail(e.target.value);
+  }
+
+  function handlePassword(e: any) {
+    setPassword(e.target.value);
+  }
 
   return (
     <IonPage>
       <IonContent>
         <Logo height="50vh" />
+
         <IonList className="login-list">
           <IonItem color="light">
-            <IonLabel position="floating">Email</IonLabel>
-            <IonInput type="email" value={email} onIonChange={handleEmail}></IonInput>
+            <IonLabel position="floating">
+              Email
+            </IonLabel>
+
+            <IonInput type="email" value={email} onIonChange={handleEmail} />
           </IonItem>
+
           <IonItem color="light">
-            <IonLabel position="floating">Password</IonLabel>
+            <IonLabel position="floating">
+              Password
+            </IonLabel>
+
             <IonInput type="password" value={password} onIonChange={handlePassword}></IonInput>
           </IonItem>
+
           <IonButton color="success" fill="outline" className="login-btn" onClick={submit}>
             LogIn
-              <IonIcon slot="end" ios={arrowForward} md={arrowForward} />
+
+            <IonIcon slot="end" ios={arrowForward} md={arrowForward} />
           </IonButton>
+
           <IonItem routerLink="/register" className="register-route">
-            <IonLabel color="secondary">Don't have an account? Register</IonLabel>
+            <IonLabel color="secondary">
+              Don't have an account? Register
+            </IonLabel>
           </IonItem>
         </IonList>
       </IonContent>
     </IonPage>
   );
-};
-
-export default Login;
+})
