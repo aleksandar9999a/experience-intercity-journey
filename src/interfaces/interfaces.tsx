2baa6daa65ab1ea75c71c19b70f430df8665ec22
Alexander Todorov <aleksandar9999a@gmail.com>
2021-02-28 12:28:19 +0200

    Added interfaces

--- src/interfaces/interfaces.tsx
+++ src/interfaces/interfaces.tsx
@@ -1,6 +1,21 @@
+import { AuthManager } from "../services/AuthManager";
+
 export interface IQuery {
   per_page?: number,
   page?: number,
   q?: string,
   key?: string
+}
+
+export interface IRoute {
+  id: number,
+  path: string,
+  Component: React.FC<any>,
+  props?: {
+    [key: string]: any
+  }
+}
+
+export interface ILoginProps {
+  authManager: AuthManager
 }
